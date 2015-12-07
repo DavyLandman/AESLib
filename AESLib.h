b00f1f83984171e1ce6ac28c50175e0a65e58232
yids <yids@disa>
2015-12-07 19:49:34 +0100

    fixed 192 bit cbc functions

--- AESLib.h
+++ AESLib.h
@@ -25,6 +25,10 @@ extern "C"{
 // key and iv are assumed to be both 128bit thus 16 uint8_t's
 void aes128_cbc_enc(const uint8_t* key, const uint8_t* iv, void* data, const uint16_t data_len);
 
+// encrypt multiple blocks of 128bit data, data_len but be mod 16
+// key and iv are assumed to be both 192bit thus 24 uint8_t's
+void aes192_cbc_enc(const uint8_t* key, const uint8_t* iv, void* data, const uint16_t data_len);
+
 // encrypt single 128bit block. data is assumed to be 16 uint8_t's
 // key is assumed to be 128bit thus 16 uint8_t's
 void aes128_enc_single(const uint8_t* key, void* data);
@@ -39,16 +43,31 @@ typedef void* aes_context;
 // key and iv are assumed to be both 128bit thus 16 uint8_t's
 aes_context aes128_cbc_enc_start(const uint8_t* key, const void* iv);
 
+// prepare an encrypted to use for encrypting multiple blocks lateron.
+// key and iv are assumed to be both 192bit thus 24 uint8_t's
+aes_context aes192_cbc_enc_start(const uint8_t* key, const void* iv);
+
 // encrypt one or more blocks of 128bit data
 // data_len should be mod 16
 void aes128_cbc_enc_continue(const aes_context ctx, void* data, const uint16_t data_len);
 
+// encrypt one or more blocks of 128bit data
+// data_len should be mod 16
+void aes192_cbc_enc_continue(const aes_context ctx, void* data, const uint16_t data_len);
+
 // cleanup encryption context
 void aes128_cbc_enc_finish(const aes_context ctx);
+
+// cleanup encryption context
+void aes192_cbc_enc_finish(const aes_context ctx);
 
 // decrypt multiple blocks of 128bit data, data_len but be mod 16
 // key and iv are assumed to be both 128bit thus 16 uint8_t's
 void aes128_cbc_dec(const uint8_t* key, const uint8_t* iv, void* data, const uint16_t data_len);
+
+// decrypt multiple blocks of 128bit data, data_len but be mod 16
+// key and iv are assumed to be both 192bit thus 24 uint8_t's
+void aes192_cbc_dec(const uint8_t* key, const uint8_t* iv, void* data, const uint16_t data_len);
 
 // decrypt single 128bit block. data is assumed to be 16 uint8_t's
 // key is assumed to be 128bit thus 16 uint8_t's
@@ -62,12 +81,24 @@ void aes256_dec_single(const uint8_t* key, void* data);
 // key and iv are assumed to be both 128bit thus 16 uint8_t's
 aes_context aes128_cbc_dec_start(const uint8_t* key, const void* iv);
 
+// prepare an decrypter to use for decrypting multiple blocks lateron.
+// key and iv are assumed to be both 192bit thus 24 uint8_t's
+aes_context aes192_cbc_dec_start(const uint8_t* key, const void* iv);
+
 // decrypt one or more blocks of 128bit data
 // data_len should be mod 16
 void aes128_cbc_dec_continue(const aes_context ctx, void* data, const uint16_t data_len);
 
+// decrypt one or more blocks of 128bit data
+// data_len should be mod 16
+void aes192_cbc_dec_continue(const aes_context ctx, void* data, const uint16_t data_len);
+
 // cleanup decryption context
 void aes128_cbc_dec_finish(const aes_context ctx);
+
+// cleanup decryption context
+void aes192_cbc_dec_finish(const aes_context ctx);
+
 #ifdef __cplusplus
 }
 #endif
